4e6c2f55bcd5ddaf58f46624ead10bce2bb4f8ec
Golgoroth22 <falinv22@gmail.com>
2020-09-29 10:35:15 +0700

    [UPD] Рефакторинг. Переименовал POJO класс

--- app/src/main/java/com/vfalin/jet/network/pojo/MemberResponse.kt
+++ app/src/main/java/com/vfalin/jet/network/pojo/MemberResponse.kt
@@ -2,7 +2,7 @@ package com.vfalin.jet.network.pojo
 
 import com.squareup.moshi.Json
 
-data class UserResponse(
+data class MemberResponse(
     @Json(name = "_id") val id: String,
     @Json(name = "status") val status: String,
     @Json(name = "name") val name: String,
--- app/src/main/java/com/vfalin/jet/network/pojo/MembersResponse.kt
+++ app/src/main/java/com/vfalin/jet/network/pojo/MembersResponse.kt
@@ -3,7 +3,7 @@ package com.vfalin.jet.network.pojo
 import com.squareup.moshi.Json
 
 data class MembersResponse(
-    @Json(name = "members") val members: List<UserResponse>,
+    @Json(name = "members") val members: List<MemberResponse>,
     @Json(name = "count") val count: Int,
     @Json(name = "offset") val offset: Int,
     @Json(name = "total") val total: Long,
